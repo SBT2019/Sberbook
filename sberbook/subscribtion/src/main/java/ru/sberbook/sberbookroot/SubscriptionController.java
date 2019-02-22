495e5c4b75fda08074236384bf3a9eecd9b1b4c5
Maxim Yakimchikov <villelouis32@gmail.com>
2019-02-22 18:44:05 +0300

    test commit

--- sberbook/subscribtion/src/main/java/ru/sberbook/sberbookroot/SubscriptionController.java
+++ sberbook/subscribtion/src/main/java/ru/sberbook/sberbookroot/SubscriptionController.java
@@ -3,7 +3,6 @@ package ru.sberbook.sberbookroot;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 
