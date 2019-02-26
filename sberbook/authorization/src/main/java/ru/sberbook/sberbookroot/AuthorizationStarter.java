068efcec2d78a0118a8b51b8ac72ac6f5e99d8fe
SheepIsland <violettasheep@gmail.com>
2019-02-27 02:51:27 +0300

    initial + controller

---
+++ sberbook/authorization/src/main/java/ru/sberbook/sberbookroot/AuthorizationStarter.java
@@ -0,0 +1,11 @@
+package ru.sberbook.sberbookroot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AuthorizationStarter {
+	public static void main(String[] args) {
+		SpringApplication.run(AuthorizationStarter.class, args);
+	}
+}
