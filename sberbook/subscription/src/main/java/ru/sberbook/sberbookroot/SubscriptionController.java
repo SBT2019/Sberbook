a73e4b3564872f3b8f0c6a365842431dfd912c3c
Alexey <Krylov@gmail.com>
2019-03-01 00:22:28 +0300

    get,add,delete methods added

--- sberbook/subscription/src/main/java/ru/sberbook/sberbookroot/SubscriptionController.java
+++ sberbook/subscription/src/main/java/ru/sberbook/sberbookroot/SubscriptionController.java
@@ -8,8 +8,18 @@ import java.util.List;
 
 @RestController
 public class SubscriptionController {
-    @GetMapping("/getSubscription")
-    public List<Long> getSubscription(long userId) {
-        return Arrays.asList(1L, 2L, 3L);
+    @GetMapping("/getAllSubscriptions")
+    public List<Long> getAllSubscriptions(long userId) {
+        return Arrays.asList(1L, 2L, 3L, 4L, 5L, 6L);
+    }
+
+    @GetMapping("/addSubscription")
+    public void addSubscription(long userId, Object subscription) {
+    }
+
+    @GetMapping("/deleteSubscription")
+    public void deleteSubscription(long userId, long subscriptionId) {
     }
 }
+
+
