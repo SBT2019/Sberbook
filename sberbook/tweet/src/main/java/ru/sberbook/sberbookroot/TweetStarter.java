a458f10b480f1cb632ec29759d9b0f666f0e5486
ABoK4Do <alesha148@gmail.com>
2019-03-01 16:43:40 +0300

    initial tweet module

---
+++ sberbook/tweet/src/main/java/ru/sberbook/sberbookroot/TweetStarter.java
@@ -0,0 +1,11 @@
+package ru.sberbook.sberbookroot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TweetStarter {
+	public static void main(String[] args) {
+		SpringApplication.run(TweetStarter.class, args);
+	}
+}
