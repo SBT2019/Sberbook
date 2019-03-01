4d17cfad554527eaab3560bae5a5d3915854ccfb
villelouis <villelouis32@gmail.com>
2019-03-02 00:40:35 +0300

    Update app.py

--- sberbook/recommendations/app.py
+++ sberbook/recommendations/app.py
@@ -12,4 +12,4 @@ def hello_world():
         return jsonify({"answer":"please type integer id"})
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=9200, debug=True)
+    app.run(host='0.0.0.0', port=9600, debug=True)
