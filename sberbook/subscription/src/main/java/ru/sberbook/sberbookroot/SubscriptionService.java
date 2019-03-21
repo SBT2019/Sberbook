bfb131faea361986bd35fa7fb888218b3ad9891f
Alexey Krylov <kakalex208@gmail.com>
2019-03-22 00:27:24 +0300

    Subscription Service, Repository added

---
+++ sberbook/subscription/src/main/java/ru/sberbook/sberbookroot/SubscriptionService.java
@@ -0,0 +1,39 @@
+package ru.sberbook.sberbookroot;
+
+import org.springframework.stereotype.Service;
+
+import java.util.Arrays;
+import java.util.List;
+
+@Service
+public class SubscriptionService {
+    private SubscriptionsRepository subscriptionsRepository;
+    private SubscribersRepository subscribersRepository;
+
+    public SubscriptionService(SubscriptionsRepository subscriptionsRepository,SubscribersRepository subscribersRepository){
+        this.subscriptionsRepository = subscriptionsRepository;
+        this.subscribersRepository = subscribersRepository;
+    }
+
+    public List<Long> getAllSubscriptions(String userId) {
+        return Arrays.asList(1L, 2L, 3L, 4L, 5L, 6L);
+    }
+
+    public List<Long> getAllSubscribers(String userId) {
+        return Arrays.asList(1L, 2L, 3L, 4L, 5L, 6L);
+    }
+
+    public void addSubscription(String userId, String subscriptionId) {
+    }
+
+    public void addSubscriber(String userId, String subscriberId) {
+    }
+
+    public void deleteSubscription(String userId, String subscriptionId) {
+    }
+
+    public void deleteSubscriber(String userId, String subscriberId) {
+    }
+
+
+}
