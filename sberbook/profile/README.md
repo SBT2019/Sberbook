3988c9eca78e799c80eec27da0db2d78e1d0ce61
slav9n4ik <slava.tresnevv@gmail.com>
2019-03-29 18:49:30 +0300

    - Ui - postgress + h2

--- sberbook/profile/README.md
+++
@@ -1,7 +0,0 @@
-**Запуск:**
-
-Перед запуском:
-1) _gradle clean build_
-2) _npm run-script dev_
-
-
--- sberbook/profile/src/main/java/ru/sberbook/sberbookroot/ProfileStarter.java
+++ sberbook/profile/src/main/java/ru/sberbook/sberbookroot/ProfileStarter.java
@@ -2,9 +2,6 @@ package ru.sberbook.sberbookroot;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.ConfigurableApplicationContext;
-import ru.sberbook.sberbookroot.dto.ProfileDto;
-import ru.sberbook.sberbookroot.service.ProfileServiceImpl;
 
 @SpringBootApplication
 public class ProfileStarter {
