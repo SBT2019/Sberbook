655670ce99efaed392305fa426b90d99a5232147
slav9n4ik <slava.tresnevv@gmail.com>
2019-04-05 11:28:13 +0300

    reset commit

--- sberbook/profile/src/main/java/ru/sberbook/sberbookroot/ProfileStarter.java
+++ sberbook/profile/src/main/java/ru/sberbook/sberbookroot/ProfileStarter.java
@@ -3,6 +3,7 @@ package ru.sberbook.sberbookroot;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+
 @SpringBootApplication
 public class ProfileStarter {
 	public static void main(String[] args) {
