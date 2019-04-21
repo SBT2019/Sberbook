a8adcde0004ce4c97f139501de83278c9a964634
Alexey Krylov <kakalex208@gmail.com>
2019-04-22 00:03:38 +0300

    small changes

--- sberbook/core/subscription/src/main/java/ru/sberbook/sberbookroot/repositories/SubscriberRepository.java
+++ sberbook/core/subscription/src/main/java/ru/sberbook/sberbookroot/repositories/SubscriberRepository.java
@@ -1,11 +1,13 @@
 package ru.sberbook.sberbookroot.repositories;
 
 import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
 import ru.sberbook.sberbookroot.entities.SubscribersEntity;
 import ru.sberbook.sberbookroot.entities.SubscribtionsEntity;
 
 import java.util.List;
 
+@Repository
 public interface SubscriberRepository extends CrudRepository<SubscribersEntity,Long> {
     List<SubscribtionsEntity> findByUserId(Long userId);
 
--- sberbook/core/subscription/src/main/java/ru/sberbook/sberbookroot/repositories/SubscriptionRepository.java
+++ sberbook/core/subscription/src/main/java/ru/sberbook/sberbookroot/repositories/SubscriptionRepository.java
@@ -1,10 +1,12 @@
 package ru.sberbook.sberbookroot.repositories;
 
-        import org.springframework.data.repository.CrudRepository;
-        import ru.sberbook.sberbookroot.entities.SubscribtionsEntity;
+import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
+import ru.sberbook.sberbookroot.entities.SubscribtionsEntity;
 
-        import java.util.List;
+import java.util.List;
 
+@Repository
 public interface SubscriptionRepository extends CrudRepository<SubscribtionsEntity,Long> {
 
     List<SubscribtionsEntity> findByUserId(Long userId);
