b61746ea41b6512192dacc16d653ff163dd4cbc9
Alexey Krylov <kakalex208@gmail.com>
2019-04-26 20:19:48 +0300

    import optimized

--- sberbook/core/subscription/src/main/java/ru/sberbook/sberbookroot/repositories/SubscriptionRepository.java
+++ sberbook/core/subscription/src/main/java/ru/sberbook/sberbookroot/repositories/SubscriptionRepository.java
@@ -1,15 +1,15 @@
 package ru.sberbook.sberbookroot.repositories;
 
-import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
 import ru.sberbook.sberbookroot.entities.SubscribtionsEntity;
 
 import java.util.List;
 
 @Repository
-public interface SubscriptionRepository extends JpaRepository<SubscribtionsEntity, Long> {
+public interface SubscriptionRepository extends CrudRepository<SubscribtionsEntity, Long> {
 
     List<SubscribtionsEntity> findByUserId(Long userId);
 
-    void deleteSubscribtionsEntityByUserIdAndSubscriptionId(Long userId, Long subscriptionId);
+    SubscribtionsEntity findSubscribtionsEntitiesByUserIdAndSubscriptionId(Long userId, Long subscriberId);
 }
